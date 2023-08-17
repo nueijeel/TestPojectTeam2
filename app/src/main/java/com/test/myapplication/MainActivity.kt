678d77b1370dd9fc11076f67c77af236fcd29f45
Ganghyungoo <104668071+Ganghyungoo@users.noreply.github.com>
2023-08-17 16:37:54 +0900

    feat: Mainactivity->activity2이동 구현(3_move_activity2)

--- app/src/main/java/com/test/myapplication/MainActivity.kt
+++ app/src/main/java/com/test/myapplication/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.test.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.test.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+        activityMainBinding.run {
+            moveActivity2.setOnClickListener {
+                startActivity(Intent(this@MainActivity,MainActivity2::class.java))
+            }
+        }
     }
 }
