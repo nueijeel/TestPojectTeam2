b1f68827e161e6917bd3d6d18620c4c70ad33016
JangYongJin <jjin9261@naver.com>
2023-08-17 16:43:38 +0900

    feat: 텍스트뷰 변경 버튼 클릭 시 텍스트뷰 변경(7_change_textview)
    버튼 누르면 텍스트뷰가 바뀝니다.

--- app/src/main/java/com/test/myapplication/MainActivity.kt
+++ app/src/main/java/com/test/myapplication/MainActivity.kt
@@ -2,10 +2,20 @@ package com.test.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.test.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        activityMainBinding.run {
+            buttonMainChangeTextView.setOnClickListener {
+                textViewMainShowText.setText("Hello World!가 아닌 Hell World!")
+            }
+        }
     }
 }
