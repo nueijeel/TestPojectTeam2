2bc8db027f4cd9c1b12c0eb9186c8fd8d0ca9012
DESKTOP-2IJ6G78\gilju <giljurha93@gmail.com>
2023-08-18 09:05:42 +0900

    feat: editText, button 추가(#5_board_write)

--- app/src/main/java/com/test/myapplication/MainActivity.kt
+++ app/src/main/java/com/test/myapplication/MainActivity.kt
@@ -2,10 +2,23 @@ package com.test.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.test.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var activityMainBindg: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        activityMainBindg = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBindg.root)
+
+        activityMainBindg.run {
+            button.run {
+                setOnClickListener {
+                    activityMainBindg.textView.text = "버튼"
+                }
+            }
+        }
     }
 }
